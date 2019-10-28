7e127cfe19c3bdf091c93b8290617a4afcac5e06
amxra <amiradediran@gmail.com>
2019-10-28 14:22:29 +0100

    Create listener

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ const db = require('./data/db')
 
 function handleDefaultRequest(req, res) {
     res.json('hello world')
-  }
+}
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on ' + (process.env.PORT || 3000))
+})
