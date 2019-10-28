a8575d61f4e6e3033adcab06cef5644dc52f0b0b
amxra <amiradediran@gmail.com>
2019-10-28 13:22:01 +0100

    create post endpoint for user ingotmation sent inside request body

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+//endpoints
+
+app.post('/api/users', (req, res) => {
+    const user = {
+        name: req.body.name,
+        bio: req.body.bio,
+        created_at: req.body.created_at,
+        updated_at: req.body.updated_at
+    }
+
+    db.insert(user)
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('listening on' + (process.env.PORT || 3000))
+})
